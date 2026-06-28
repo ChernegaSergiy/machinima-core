576bcc81e7b4e5b33f74e4b38eb8b333417f97a9
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-06-28 16:20:58 +0300

    fix: Change Route from Annotation to Attribute to resolve RouteNotFoundException

--- src/Controller/AvatarController.php
+++ src/Controller/AvatarController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
