2321bd0f5a8162c514d0a3d0174e6cdf8e117f19
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-06-28 19:39:18 +0300

    feat: Mark notification as read when clicked before redirecting

--- src/Controller/Api/NotificationController.php
+++ src/Controller/Api/NotificationController.php
@@ -23,4 +23,27 @@ class NotificationController extends AbstractController
 
         return $this->json(['success' => true]);
     }
+
+    #[Route('/notifications/{id}/redirect', name: 'app_notification_redirect', methods: ['GET'])]
+    public function readAndRedirect(int $id, EntityManagerInterface $em): \Symfony\Component\HttpFoundation\Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_index');
+        }
+
+        $notification = $em->getRepository(\App\Entity\Notification::class)->find($id);
+        if ($notification && $notification->getUser() === $user) {
+            if (!$notification->isRead()) {
+                $notification->setIsRead(true);
+                $em->flush();
+            }
+
+            if ($notification->getTargetId()) {
+                return $this->redirectToRoute('app_post', ['id' => $notification->getTargetId()]);
+            }
+        }
+
+        return $this->redirectToRoute('app_notifications');
+    }
 }
