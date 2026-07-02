aaecec7313e7f91648be294ed8055e63181609a2
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-02 14:55:43 +0300

    fix: Update entity from Post to Content in CandidateGeneratorInterface.php

--- src/Service/Recommendation/Generator/CandidateGeneratorInterface.php
+++ src/Service/Recommendation/Generator/CandidateGeneratorInterface.php
@@ -11,7 +11,7 @@ interface CandidateGeneratorInterface
     /**
      * @param User|null $user The current user (null if guest)
      * @param int $limit Maximum number of posts to fetch
-     * @return array<int, \App\Entity\Post>
+     * @return array<int, \App\Entity\Content>
      */
     public function generate(?User $user, int $limit = 50): array;
 }
