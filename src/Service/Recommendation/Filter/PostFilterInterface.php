51b6a6ea22565f273171a94eb68b82b973dcfa5b
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-02 14:50:39 +0300

    refactor: Add AutoconfigureTag to PostFilterInterface

--- src/Service/Recommendation/Filter/PostFilterInterface.php
+++ src/Service/Recommendation/Filter/PostFilterInterface.php
@@ -4,7 +4,9 @@ namespace App\Service\Recommendation\Filter;
 
 use App\Entity\User;
 use App\Service\Recommendation\DTO\CandidatePost;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
+#[AutoconfigureTag('app.post_filter')]
 interface PostFilterInterface
 {
     /**
