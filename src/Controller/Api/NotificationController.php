f91a0004370bd573a0820823384aeb7acf02866e
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-03 21:24:30 +0300

    refactor: Thin NotificationController via DbNotificationService

--- src/Controller/Api/NotificationController.php
+++ src/Controller/Api/NotificationController.php
@@ -2,72 +2,47 @@
 
 namespace App\Controller\Api;
 
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\Notification\DbNotificationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class NotificationController extends AbstractController
 {
+    public function __construct(
+        private DbNotificationService $dbNotificationService,
+    ) {
+    }
+
     #[Route('/api/notifications/read', name: 'api_notifications_read', methods: ['POST'])]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function markAllAsRead(EntityManagerInterface $em): JsonResponse
+    public function markAllAsRead(): JsonResponse
     {
-        /** @var \App\Entity\User $user */
-        $user = $this->getUser();
-
-        $em->createQuery('UPDATE App\Entity\Notification n SET n.isRead = true WHERE n.user = :user AND n.isRead = false')
-           ->setParameter('user', $user)
-           ->execute();
+        $this->dbNotificationService->markAllAsRead($this->getUser());
 
         return $this->json(['success' => true]);
     }
 
     #[Route('/api/notifications/unread-count', name: 'api_notifications_unread_count', methods: ['GET'])]
-    public function unreadCount(EntityManagerInterface $em): JsonResponse
+    public function unreadCount(): JsonResponse
     {
-        $user = $this->getUser();
-        if (!$user) {
-            return $this->json(['count' => 0]);
-        }
-        $count = $em->getRepository(\App\Entity\Notification::class)->count(['user' => $user, 'isRead' => false]);
+        $count = $this->dbNotificationService->getUnreadCount($this->getUser());
 
         return $this->json(['count' => $count]);
     }
 
     #[Route('/notifications/{id}/redirect', name: 'app_notification_redirect', methods: ['GET'])]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function readAndRedirect(int $id, EntityManagerInterface $em): \Symfony\Component\HttpFoundation\Response
+    public function readAndRedirect(int $id): Response
     {
-        /** @var \App\Entity\User $user */
-        $user = $this->getUser();
+        $redirect = $this->dbNotificationService->markAsReadAndGetRedirect($id, $this->getUser());
 
-        $notification = $em->getRepository(\App\Entity\Notification::class)->find($id);
-        if ($notification && $notification->getUser() === $user) {
-            if (!$notification->isRead()) {
-                $notification->setIsRead(true);
-                $em->flush();
-            }
-
-            if ($notification->getTargetId() && $notification->getTargetType()) {
-                switch ($notification->getTargetType()) {
-                    case 'post':
-                        return $this->redirectToRoute('app_post', ['id' => $notification->getTargetId()]);
-                    case 'comment':
-                        $comment = $em->getRepository(\App\Entity\Comment::class)->find($notification->getTargetId());
-                        if ($comment && $comment->getContent()) {
-                            return $this->redirect($this->generateUrl('app_post', ['id' => $comment->getContent()->getId()]).'?scrollTo=comment-item-'.$comment->getId());
-                        }
-                        break;
-                    case 'author':
-                        return $this->redirectToRoute('app_author', ['id' => $notification->getTargetId()]);
-                    case 'category':
-                        return $this->redirectToRoute('app_category', ['id' => $notification->getTargetId()]);
-                }
-            }
+        if (!$redirect) {
+            return $this->redirectToRoute('app_notifications');
         }
 
-        return $this->redirectToRoute('app_notifications');
+        return $this->redirectToRoute($redirect['route'], $redirect['params']);
     }
 }
