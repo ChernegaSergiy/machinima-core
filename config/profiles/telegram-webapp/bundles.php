8c8ecce8cae82ffef3f449756dfbaa95d4968573
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-05 18:19:08 +0300

    feat: Register Telegram adapter bundle and fix login screen for OAuth

--- config/profiles/telegram-webapp/bundles.php
+++ config/profiles/telegram-webapp/bundles.php
@@ -2,4 +2,5 @@
 
 return [
     Morfeditorial\TelegramBotBundle\TelegramBotBundle::class => ['all' => true],
+    Morfeditorial\MachinimaTelegramAdapter\MachinimaTelegramAdapterBundle::class => ['all' => true],
 ];
