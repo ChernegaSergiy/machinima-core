291a249e7976f8ae0f2a515b0a6843d227730b96
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-14 22:33:41 +0300

    refactor: Replace Kernel with MachinimaCoreBundle class

---
+++ src/MachinimaCoreBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Morfeditorial\MachinimaCoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MachinimaCoreBundle extends Bundle
+{
+}
