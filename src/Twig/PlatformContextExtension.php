c2f489ebd8485f01c90311145b9b142795f013c9
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-14 17:02:01 +0300

    feat: Expose ui_splash_screens function to Twig templates

--- src/Twig/PlatformContextExtension.php
+++ src/Twig/PlatformContextExtension.php
@@ -15,6 +15,7 @@ class PlatformContextExtension extends AbstractExtension
             new TwigFunction('platform_context', [PlatformUiContextRuntime::class, 'getContext']),
             new TwigFunction('platform_bootstrap_module_paths', [PlatformUiContextRuntime::class, 'getBootstrapModulePaths']),
             new TwigFunction('platform_ui_hints_module_path', [PlatformUiContextRuntime::class, 'getUiHintsModulePath']),
+            new TwigFunction('ui_splash_screens', [PlatformUiContextRuntime::class, 'getSplashScreens']),
         ];
     }
 }
